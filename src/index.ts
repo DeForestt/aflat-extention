bacd11fc151347cbb2a0d740f59e7d448d16f59f
DeForestt Thompson <deforesttthompson@gmail.com>
2022-02-08 16:16:25 -0500

    Demo Message

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
 import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+    console.log('Congratulations, your extension "aflat" is now active!');
+    vscode.window.showInformationMessage('Hello World!');
+    context.subscriptions.push(vscode.commands.registerCommand('extension.sayHello', () => {
+        vscode.window.showInformationMessage('Hello World!');
+    }));
+};
+
+export function deactivate() {};
