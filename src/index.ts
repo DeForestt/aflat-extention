a0b6c0de584d0307e13b2bbdead018143ba7c94c
DeForestt Thompson <deforesttthompson@gmail.com>
2022-02-08 19:50:23 -0500

    removed fluf

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,6 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log('Congratulations, your extension "aflat" is now active!');
-    vscode.window.showInformationMessage('Hello World!');
-    context.subscriptions.push(vscode.commands.registerCommand('aflat.sayHello', () => {
-        vscode.window.showInformationMessage('Hello World!');
-    }));
-
-    context.subscriptions.push( vscode.languages.registerHoverProvider('aflat', {
-            provideHover(document, position, token) {
-                const range = document.getWordRangeAtPosition(position);
-                const word = document.getText(range);
-                return new vscode.Hover(`Hello ${word}`);
-            }
-    }));
 };
 
 export function deactivate() {};
